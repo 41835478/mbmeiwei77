b376d1db96dcecbe91f4ad8863f8e84174a6ba11
hui.liu <980500610@qq.com>
2015-01-07 14:04:03 +0800

    价格属性选择

--- js/common.js
+++ js/common.js
@@ -42,8 +42,8 @@ function addToCart(goodsId, parentId)
   // 检查是否有商品规格 
   if (formBuy)
   {
-    spec_arr = getSelectedAttributes(formBuy);
-
+    //spec_arr = getSelectedAttributes(formBuy);
+    spec_arr = getSelectedAttributesBySelect(formBuy);
     if (formBuy.elements['number'])
     {
       number = formBuy.elements['number'].value;
@@ -105,6 +105,28 @@ function getSelectedAttributes(formBuy)
       formBuy.elements[i].tagName == 'SELECT'))
     {
       spec_arr[j] = formBuy.elements[i].value;
+      j++ ;
+    }
+  }
+
+  return spec_arr;
+}
+
+/**
+ * 获得选定的商品属性 define
+ */
+function getSelectedAttributesBySelect(formBuy)
+{
+  var spec_arr = new Array();
+  var j = 0;
+
+  for (i = 0; i < formBuy.elements.length; i ++ )
+  {
+    var prefix = formBuy.elements[i].name.substr(0, 5);
+
+    if (prefix == 'spec_' && formBuy.elements[i].type=='button'&&formBuy.elements[i].className=='active')
+    {
+      spec_arr[j] = formBuy.elements[i].id;
       j++ ;
     }
   }
